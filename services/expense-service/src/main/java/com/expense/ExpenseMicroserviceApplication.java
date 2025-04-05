f4540e7a632e43d431962652f904928b8d04bf9e
Palak921 <palakparihar144@gmail.com>
2025-04-05 14:51:43 +0530

    Add expense-microservice

---
+++ services/expense-service/src/main/java/com/expense/ExpenseMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMicroserviceApplication.class, args);
+	}
+
+}
